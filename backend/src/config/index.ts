ef822d38cd5774ac3662d4be764f9d017e343eff
ShuvoProgram <shuvoprogramer@gmail.com>
2024-07-14 13:11:41 -0700

    📄 Define pagination fields array

---
+++ backend/src/config/index.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.join(process.cwd(), '.env') })
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 5001,
+  database_url: process.env.DATABASE_URL,
+  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  }
+}
